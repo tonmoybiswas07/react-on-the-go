9cef4b2b04397fec91e95506e516393717a291f4
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-04 02:32:46 +0600

    updated

--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -3,24 +3,35 @@ import type CountryType from "../../Type";
 import "./Country.css";
 
 export interface CountryProps {
-  country:CountryType;
-  handleVisitedFlag:(flag:string)=>void
+  country: CountryType;
+  handleVisitedFlag: (flag: string) => void;
 }
 
-export default function Country({ country ,handleVisitedFlag}: CountryProps) {
-  const [visited, setVisited] = useState<Boolean>(false);
-  const handleVisited = (country) => {
+export default function Country({
+  country,
+  handleVisitedFlag,
+}: CountryProps) {
+  const [visited, setVisited] = useState<boolean>(false);
+
+  const handleVisited = () => {
     setVisited(!visited);
-    handleVisitedFlag(country);
+    handleVisitedFlag(country.flags.flags.png);
   };
+
   return (
-    <div className={`${visited?"visited-country":"country"}`}>
+    <div className={visited ? "visited-country" : "country"}>
       <h3>{country.name.common}</h3>
-      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+
+      <img
+        src={country.flags.flags.png}
+        alt={country.flags.flags.alt}
+      />
+
       <div className="information">
         <p>Population: {country.capital.capital}</p>
         <p>Capital: {country.population.population}</p>
       </div>
+
       <button onClick={handleVisited}>
         {visited ? "visited" : "mark as read"}
       </button>
