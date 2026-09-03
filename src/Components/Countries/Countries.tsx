509ad590e37400fa37845f970277750447912e03
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-04 00:43:50 +0600

    update react on the go

--- src/Components/Countries/Countries.tsx
+++ src/Components/Countries/Countries.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type CountryType from "../../Type";
 import Country from "../Country/Country";
 import "./Countries.css";
@@ -11,12 +11,30 @@ export default function Countries({ CountriesPromise }: CountriesProps) {
   const countries = use(CountriesPromise);
   console.log(countries);
 
+  const [visitedFlag, setVisitedFlag] = useState<string[]>([]);
+
+  const handleVisitedFlag = (flag: string): void => {
+    if (visitedFlag.includes(flag)) {
+      
+        const remainigFlags= visitedFlag.filter(f=>f!==flag)
+        setVisitedFlag(remainigFlags)
+    } else {
+      const newVisitedFlags = [...visitedFlag, flag];
+      setVisitedFlag(newVisitedFlags);
+    }
+  };
+
   return (
     <div>
       <h2>Countries:{countries.length}</h2>
+      <h4>Visited Flags:{visitedFlag.length}</h4>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country}></Country>
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisitedFlag={handleVisitedFlag}
+          ></Country>
         ))}
       </div>
     </div>
--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import type CountryType from "../../Type";
 import "./Country.css";
 
 export interface CountryProps {
   country: Promise<CountryType[]>;
+  handleVisitedFlag:(flag:string)=>void
 }
 
-export default function Country({ country }: CountryProps) {
+export default function Country({ country ,handleVisitedFlag}: CountryProps) {
+  const [visited, setVisited] = useState<Boolean>(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+    handleVisitedFlag(country)
+  };
   return (
-    <div className="country">
+    <div className={`${visited?"visited-country":"country"}`}>
       <h3>{country.name.common}</h3>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <div className="information">
         <p>Population: {country.capital.capital}</p>
         <p>Capital: {country.population.population}</p>
       </div>
+      <button onClick={handleVisited}>
+        {visited ? "visited" : "mark as read"}
+      </button>
     </div>
   );
 }
